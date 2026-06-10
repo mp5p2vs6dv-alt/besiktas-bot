125083db12d00c0270b4a6e6f7afcdc9b3eb2723
mp5p2vs6dv-alt <mp5p2vs6dv@privaterelay.appleid.com>
2026-06-10 22:24:00 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import feedparser
+import tweepy
+import time
+import os
+
+API_KEY = os.environ.get("API_KEY")
+API_SECRET = os.environ.get("API_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+
+client = tweepy.Client(
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
+posted = set()
+
+def check_and_post():
+    feed = feedparser.parse("https://news.google.com/rss/search?q=Besiktas&hl=tr&gl=TR")
+    for article in feed.entries[:5]:
+        if article.link not in posted:
+            tweet = f"⚫⚪ {article.title}\n\n{article.link}"
+            client.create_tweet(text=tweet[:280])
+            posted.add(article.link)
+            print(f"Posted: {article.title}")
+            time.sleep(10)
+
+while True:
+    check_and_post()
+    print("Waiting 1 hour...")
+    time.sleep(3600)
